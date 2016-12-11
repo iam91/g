1335f2983779fe44c29ec5d87c6b364a5ec23060
iam91 <zhengwy91@foxmail.com>
2016-12-11 09:15:44 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 window.onload = function(){
 
+	data1 = [
+        {value:100, name:''},
+        {value:105, name:''},
+        {value:110, name:''},
+        {value:108, name:''},
+        {value:105, name:''},
+	];
+
+	data2 = [
+        {value:100, name:''},
+        {value:105, name:''},
+        {value:110, name:''},
+        {value:105, name:''},
+        {value:108, name:''},
+        {value:112, name:''},
+        {value:110, name:''},
+        {value:105, name:''},
+        {value:111, name:''}
+	];
+
+	data3 = [
+        {value:100, name:''},
+        {value:95, name:''},
+        {value:102, name:''},
+        {value:98, name:''},
+        {value:103, name:''},
+	];
+
 	pie1 = {
             name:'',
             type:'pie',
             radius : '55%',
-            center: ['45%', '40%'],
-            data:[
-                {value:335, name:''},
-                {value:310, name:''},
-                {value:274, name:''},
-                {value:235, name:''},
-                {value:400, name:''}
-            ].sort(function (a, b) { return a.value - b.value}),
+            center: ['50%', '50%'],
+            data: data1,
             roseType: 'angle',
             label: {
                 normal: {
@@ -39,42 +61,16 @@ window.onload = function(){
             }
         };
 
-   	pie2 = {
-            name:'',
-            type:'pie',
-            radius : '45%',
-            center: ['50%', '50%'],
-            data:[
-                {value:274, name:''},
-                {value:235, name:''},
-                {value:400, name:''}
-            ].sort(function (a, b) { return a.value - b.value}),
-            roseType: 'angle',
-            label: {
-                normal: {
-                    textStyle: {
-                        color: 'rgba(255, 255, 255, 0.3)'
-                    }
-                }
-            },
-            labelLine: {
-                normal: {
-                    lineStyle: {
-                        color: '#2c343c'
-                    },
-                    smooth: 0.2,
-                    length: 10,
-                    length2: 20
-                }
-            },
-            itemStyle: {
-                normal: {
-                    color: '#c23531',
-                    shadowBlur: 200,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
-        };
+    var pie2 = {};
+    var pie3 = {};
+    for(key in pie1){
+    	pie2[key] = pie1[key];
+    	pie3[key] = pie1[key];
+    }
+    pie2.data = data2;
+    pie2.radius = '45%';
+    pie3.data = data3;
+    pie3.radius = '35%';
 
 	option = {
 	    backgroundColor: '#2c343c',
@@ -105,7 +101,7 @@ window.onload = function(){
 	        }
 	    },
 	    series : [
-	    	pie1,pie2
+	    	pie1, pie2, pie3
 	    ]
 	};
 
